c1b6eb70dc1c5e0ace1f182cee74524419b5a1b3
Triani, Mario <Mario.Triani@fao.org>
2016-06-20 15:04:16 +0200

    New Metadataeditor config

--- config/submodules/datamng/config.js
+++ config/submodules/datamng/config.js
@@ -34,6 +34,71 @@ define(['jquery', 'fx-submodules/config/baseConfig'],
 
         cfg.SITE_TEMPLATE = 'topmenu';
 
+        cfg.METADATA_PATH = {
+            schemaPath: '../../config/submodules/metadataEditor/schemas/'
+        };
+
+
+        cfg.METADATA_SEC = [
+            {
+                id: "identification", text: "Identification", icon: "img/fenix-catalog-sprite-small.svg",
+                state: {
+                    selected: true
+                }
+            },
+            {id: "contacts", text: "Contacts"},
+            {
+                id: "content", text: "Content", children: [
+                {id: 'content_ReferencePopulation', text: "Reference Population"},
+                {id: 'content_Coverage', text: "Coverage"}
+            ]
+            },
+            {id: "institutionalMandate", text: "Institutional Mandate"},
+            {
+                id: "statisticalProcessing",
+                text: "Statistical Processing",
+                state: {disabled: true, opened: true},
+                children: [
+                    {id: 'statisticalProcessing_primaryDataCollection', text: "Primary Data Collection"},
+                    {id: 'statisticalProcessing_secondaryDataCollection', text: "Secondary Data Collection"},
+                    {id: 'statisticalProcessing_dataCompilation', text: "Data Compilation"},
+                    {id: 'statisticalProcessing_dataValidation', text: "Data Validation"}
+                ]
+            },
+            {
+                id: "dataQuality", text: "Data Quality", children: [
+                {id: "dataQuality_accuracy", text: "Accuracy"},
+                {id: "dataQuality_dataRevision", text: "Data Revision"},
+                {id: "dataQuality_relevance", text: "Relevance"},
+                {id: "dataQuality_compatibilityCoherence", text: "Compatibility Coherence"},
+                {id: "dataQuality_timelinessAndPunctuality", text: "Timeliness and Puctuality"}
+            ]
+            },
+            {
+                id: "accessibility", text: "Accessibility", state: {disabled: true, opened: true}, children: [
+                {
+                    id: "accessibility_dataDissemination",
+                    text: "Data Dissemination",
+                    state: {disabled: true, opened: true},
+                    children: [
+                        {id: "accessibility_dataDissemination_distribution", text: "Distribution"},
+                        {id: "accessibility_dataDissemination_releasePolicy", text: "Release Policy"}
+                    ],
+
+                },
+                {id: "accessibility_clarity", text: "Clarity"},
+                {id: "accessibility_confidentiality", text: "Confidentiality"}
+            ]
+            },
+            {
+                id: "maintenance", text: "Maintenance", children: [
+                {id: "maintenance_update", text: "Update"},
+                {id: "maintenance_metadataMaintenance", text: "Metadata Maintenance"}
+            ]
+            }
+            //,{ id: "documents", text: "Documents" }
+        ];
+
 
         cfg.DSD_EDITOR_CONTEXT_SYSTEM = "cstat_gmb";
         cfg.DSD_EDITOR_LABEL = "CountrySTAT Gambia"
